b691a4e0dc571143760b26e683b4d1efffc44ba8
Mukhtar Assadi <ma.assadi@student.han.nl>
2023-03-29 13:43:47 +0200

    Sprites toegevoegd

--- src/main/java/com/github/hanyaeger/tutorial/Shooter.java
+++ src/main/java/com/github/hanyaeger/tutorial/Shooter.java
@@ -1,13 +1,17 @@
 package com.github.hanyaeger.tutorial;
 
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.YaegerGame;
 
 public class Shooter extends YaegerGame {
+    public static void main(String[] args){
+        launch(args);
+    }
     @Override
     public void setupGame() {
-
+        setGameTitle("Shooter");
+        setSize(new Size(1200, 800));
     }
-
     @Override
     public void setupScenes() {
 
