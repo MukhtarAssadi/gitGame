e6f5cabed60607d7046dae85f04da1e30c983184
Mukhtar Assadi <ma.assadi@student.han.nl>
2025-05-11 22:52:49 +0200

    Loopfunctionaliteit en extra sprites toegevoegd

---
+++ src/main/java/com/github/hanyaeger/tutorial/entities/player/Player.java
@@ -0,0 +1,53 @@
+package com.github.hanyaeger.tutorial.entities.player;
+
+import com.github.hanyaeger.api.AnchorPoint;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.userinput.KeyListener;
+import javafx.scene.input.KeyCode;
+
+import java.util.Set;
+
+public class Player extends DynamicSpriteEntity implements KeyListener {
+
+    public Player(Coordinate2D location) {
+        super("sprites/player.png", location, new Size(70, 70), 1, 2);
+
+        setAnchorPoint(AnchorPoint.CENTER_CENTER);
+    }
+
+    @Override
+    public void onPressedKeysChange(Set<KeyCode> pressedKeys){
+        boolean left = pressedKeys.contains(KeyCode.LEFT);
+        boolean right = pressedKeys.contains(KeyCode.RIGHT);
+        boolean up = pressedKeys.contains(KeyCode.UP);
+        boolean down = pressedKeys.contains(KeyCode.DOWN);
+
+        if (left || right || up || down) {
+            double angle = -1;
+
+            if (up && right) {
+                angle = 135;
+            } else if (up && left) {
+                angle = 225;
+            } else if (down && right) {
+                angle = 45;
+            } else if (down && left) {
+                angle = 315;
+            } else if (up) {
+                angle = 180;
+            } else if (down) {
+                angle = 0;
+            } else if (left) {
+                angle = 270;
+            } else if (right) {
+                angle = 90;
+            }
+
+            setMotion(3, angle);
+        } else {
+            setSpeed(0);
+        }
+    }
+}
--- src/main/java/com/github/hanyaeger/tutorial/scenes/GameScene.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/GameScene.java
@@ -1,7 +1,10 @@
 package com.github.hanyaeger.tutorial.scenes;
 
+import com.github.hanyaeger.api.AnchorPoint;
+import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import com.github.hanyaeger.tutorial.Shooter;
+import com.github.hanyaeger.tutorial.entities.player.Player;
 
 public class GameScene extends DynamicScene {
 
@@ -13,12 +16,14 @@ public class GameScene extends DynamicScene {
 
     @Override
     public void setupScene() {
-//        setBackgroundAudio("audio/");
+        setBackgroundAudio("audio/ocean.mp3");
         setBackgroundImage("sprites/vloer.png");
     }
 
     @Override
     public void setupEntities() {
-
+        var player = new Player(new Coordinate2D(getWidth() / 2, getHeight() / 1.5));
+        addEntity(player);
+        player.setAnchorPoint(AnchorPoint.CENTER_CENTER);
     }
 }
